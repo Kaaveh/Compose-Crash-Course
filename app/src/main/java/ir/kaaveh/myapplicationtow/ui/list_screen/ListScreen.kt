a293f416b704f9baa48814b6c598de94901518ef
kaaveh <kaaveh@pm.me>
2022-11-22 08:50:58 +0330

    LazyColumn

---
+++ app/src/main/java/ir/kaaveh/myapplicationtow/ui/list_screen/ListScreen.kt
@@ -0,0 +1,53 @@
+package ir.kaaveh.myapplicationtow.ui.list_screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+val names = listOf("aaa", "bbb", "cccc", "dddd")
+
+@Composable
+fun ListScreen(
+    nameList: List<String> = names
+) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(36.dp),
+    ) {
+        items(items = nameList){
+            Text(text = it)
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ListScreen()
+}
+
+@Preview(
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+fun DefaultPreviewDark() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        ListScreen()
+    }
+}
