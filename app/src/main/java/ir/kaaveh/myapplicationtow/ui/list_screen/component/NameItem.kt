e538ed6a7f1718b3b97e6c75c049bad9dd7b84e9
kaaveh <kaaveh@pm.me>
2022-11-23 08:56:24 +0330

    10 LazyColumn click handler

--- app/src/main/java/ir/kaaveh/myapplicationtow/ui/list_screen/component/NameItem.kt
+++ app/src/main/java/ir/kaaveh/myapplicationtow/ui/list_screen/component/NameItem.kt
@@ -1,13 +1,16 @@
 package ir.kaaveh.myapplicationtow.ui.list_screen.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -16,10 +19,20 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun NameItem(
     name: String,
-    fontSize: TextUnit = 20.sp
+    fontSize: TextUnit = 20.sp,
 ) {
+
+    var backgroundColor by remember {
+        mutableStateOf(Color.White)
+    }
+
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor)
+            .clickable {
+                backgroundColor = Color.Red
+            }
     ) {
         Spacer(modifier = Modifier.height(4.dp))
         Text(
